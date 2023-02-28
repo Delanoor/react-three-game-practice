b1c351a194a7a8c98fbab4db71e31fbd2f408457
Delanoor Nahm <hwn0809@gmail.com>
2023-02-28 10:25:43 +0900

    first commit

---
+++ src/Experience.jsx
@@ -0,0 +1,18 @@
+import { OrbitControls } from "@react-three/drei";
+import { Physics, Debug } from "@react-three/rapier";
+import Level from "./Level.jsx";
+import Lights from "./Lights.jsx";
+
+export default function Experience() {
+    return (
+        <>
+            <OrbitControls makeDefault />
+
+            <Physics>
+                <Debug />
+                <Lights />
+                <Level />
+            </Physics>
+        </>
+    );
+}
---
+++ src/Level.jsx
@@ -0,0 +1,31 @@
+import * as THREE from "three";
+THREE.ColorManagement.legacyMode = false;
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+
+const floor1Material = new THREE.MeshStandardMaterial({ color: "limegreen" });
+const floor2Material = new THREE.MeshStandardMaterial({ color: "greenyellow" });
+const obstacleMaterial = new THREE.MeshStandardMaterial({ color: "orangered" });
+const wallMaterial = new THREE.MeshStandardMaterial({ color: "slategrey" });
+
+function BlockStart({ position = [0, 0, 0] }) {
+    return (
+        <group position={position}>
+            <mesh
+                geometry={boxGeometry}
+                material={floor1Material}
+                receiveShadow
+                position={[0, -0.1, 0]}
+                scale={[4, 0.2, 4]}
+            ></mesh>
+        </group>
+    );
+}
+
+export default function Level() {
+    return (
+        <>
+            <BlockStart position={[0, 0, 0]} />
+        </>
+    );
+}
